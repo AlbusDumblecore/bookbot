265c4c3bc6e6b31452552ae57b1937cd585ebc08
AlbusDumblecore <charles.raidron@gmail.com>
2025-01-05 11:43:40 +0100

    Premier et deuxieme exercice

--- main.py
+++ main.py
@@ -2,5 +2,20 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     print(file_contents)
+    liste_mots = file_contents.split()
+    counter = 0
+    for i in range(0,len(liste_mots)):
+       counter +=1
+    print(counter)
+    
+    liste_minuscule = liste_mots.lower()
+    listing_lettres = {}
+    for mots in liste_minuscule:
+        for character in mots:
+            if character in listing_lettres:
+                listing_lettres[character] +=1
+            else:
+                listing_lettres[character]=1
+    print(listing_lettres)
 main()
 
